0cd2b4b5f0972eadbb50fea675e766b08a3de0a8
katseyes <thibof@hotmail.com>
2022-02-24 16:50:23 +0100

    Updates

---
+++ precache-manifest.2214c2e27d4d15e7fe2824cd98adf511.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d0d8c9e342dc5c8e261bc9d1af4345",
+    "url": "/SimpleStorageTP/index.html"
+  },
+  {
+    "revision": "b9b8aca4682b6676f498",
+    "url": "/SimpleStorageTP/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "c652c52305112cc2bdab",
+    "url": "/SimpleStorageTP/static/js/2.cfb37df5.chunk.js"
+  },
+  {
+    "revision": "b9b8aca4682b6676f498",
+    "url": "/SimpleStorageTP/static/js/main.b8c12191.chunk.js"
+  },
+  {
+    "revision": "753807f7f43393945d67",
+    "url": "/SimpleStorageTP/static/js/runtime-main.9889c1b9.js"
+  }
+]);
